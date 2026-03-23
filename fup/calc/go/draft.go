dd6da7362fdbeec52b7913fefe76c1228170d2e2
Caua-Souz <cauasouza6787@gmail.com>
2026-03-23 18:17:17 +0000

    msg

--- fup/calc/go/draft.go
+++ fup/calc/go/draft.go
@@ -1,11 +1,26 @@
 package main
 import "fmt"
 func main() {
-    var num1, num2 int
+    var num1, num2, soma, subt, multi, divisao int
     var operador string
 
     fmt.Scan(&num1)
     fmt.Scan(&num2)
     fmt.Scan(&operador)
+
+    if operador == "+" {
+        soma = (num1 + num2) 
+        fmt.Println(soma) 
+    } else if operador == "-" {
+        subt = (num1 - num2)
+        fmt.Println(subt)
+    } else if operador == "*" {
+        multi = (num1 * num2)
+        fmt.Println(multi)        
+    } else if operador == "/" {
+        divisao = (num1 / num2)
+        fmt.Println(divisao)
+    } else {
     fmt.Println("Hello, World!")
+}
 }
