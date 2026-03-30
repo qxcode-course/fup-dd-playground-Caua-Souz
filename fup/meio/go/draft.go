4d1cdeeb2abe3323a538bc8ec2a935a4e2b61f27
Caua-Souz <cauasouza6787@gmail.com>
2026-03-30 17:23:54 +0000

    enviando

--- fup/meio/go/draft.go
+++ fup/meio/go/draft.go
@@ -6,11 +6,11 @@ func main() {
     fmt.Scan(&num2)
     fmt.Scan(&num3)
 
-    if num1 < num2 && num2 < num3 {
+    if num2 > num1 && num1 > num3 { //verificar se o número 1 é o do meio
+        fmt.Println(num1) 
+    } else if num1 < num2 && num2 < num3 || num1 > num2 && num2 > num3 { // verificar as condições para o número 2 ser o do meio
         fmt.Println(num2)
-    } else if num2 < num1 && num1 < num3 {
-        fmt.Println(num1)
     } else {
-    fmt.Println(num3)
-}
+        fmt.Println(num3)
+    }
 }
--- fup/round/go/draft.go
+++ fup/round/go/draft.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
-
+	"math"
 )
 func main() {
-   fmt.Println("Hello, World!")
+	var op string
+	var num, res float64
+	fmt.Scan(&op)
+	fmt.Scan(&num)
+	fmt.Scan(&res)
+
+	if op == "r" { //ler se a operação é round
+	res = math.Round(num) //usando a função round
+	fmt.Println(res)
+} else if op == "f" { //ler se a operação é floor
+	res = math.Floor(num) //usando a função floor
+	fmt.Println(res)
+} else {
+	res = math.Ceil(num) //ler se a operação é ceil
+	fmt.Println(res) //usando a função ceil
+}
 }
