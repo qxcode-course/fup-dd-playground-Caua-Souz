8f3a0b7eb60f3f201ed5f281b2025e62a5b3dafd
Caua-Souz <cauasouza6787@gmail.com>
2026-03-30 16:09:44 +0000

    enviando

--- fup/impar/go/draft.go
+++ fup/impar/go/draft.go
@@ -1,7 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello World!")
+      var p, d1, d2, dt int
+    fmt.Scan(&p)
+    fmt.Scan(&d1)
+    fmt.Scan(&d2)
+    fmt.Scan(&dt)
+
+    dt = (d1+d2)
+    if p != 1 && dt%2 == 0 {
+        fmt.Println("0")
+    } else if p != 0 && dt%2 == 1{
+        fmt.Println("0")
+    } else {
+        fmt.Println("1")
+    }
+
+  
         }
     
    
