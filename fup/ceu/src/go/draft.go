06928541f66e9bf2ef97d3a079c81851770e9f47
Caua-Souz <cauasouza6787@gmail.com>
2026-04-07 18:22:56 +0000

    07/04

--- fup/ceu/src/go/draft.go
+++ fup/ceu/src/go/draft.go
@@ -1,10 +1,18 @@
 package main
 import "fmt"
 func main() {
-    var n, i int
-    var ceu string
-    fmt.Scan(&n)
+    var npedra, i int
+    fmt.Scan(&npedra)
     i = 0
-fmt.Print("[", " ")
-    for ; i < 
+fmt.Print("[", " ") 
+    for ; i <= 9 ; i++ { 
+        if i == npedra { //vendo se o contador é igual ao número da pedra.
+            continue //se for igual, esse número não entra na contagem, logo o continue é necessário para o "skip".
+            }
+        fmt.Print(i, " ") //printar a repetição com um espaço à direita.
+    }  
+   if npedra != 10 {
+    fmt.Print("ceu ") //printar "céu" e fechar de forma devida.
+   }
+   fmt.Println("]")
 }
---
+++ fup/esconde/src/go/draft.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main() {
+    var num, i int
+    fmt.Scan(&num)
+
+   
+
+
+    for i = 1 ; i <= num; i+= 2 {
+     fmt.Println(i)
+         }
+    for i = 1 ; num > i ; i-= 2 {
+        fmt.Println(i)
+          
+          
+        }
+    }
+
